0adbde65c899089337d29f5860ef06a9c7deba0e
KM <236639@student.pwr.edu.pl>
2020-12-13 18:48:54 +0100

    functionality for displaying doctor's visits created

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from appointments.forms import PatientForm, LoginForm, AddressForm, DoctorForm, VisitForm
+from appointments.models import Visit, Address, Patient
 
 
 def register(request):
@@ -45,7 +46,8 @@ def logout(request):
 
 def home(request):
     if is_already_logged(request):
-        return render(request, redirect_template(request))
+        template_name, context = redirect_template(request)
+        return render(request, template_name, context=context)
     else:
         form = LoginForm()
         return render(request, 'login.html', {'form': form})
@@ -59,29 +61,32 @@ def is_already_logged(request):
 
 def redirect_template(request):
     if request.session.get('user_type') == 'patient':
-        return 'patient_home.html'
+        context = {}
+        template_name = 'patient_home.html'
     elif request.session.get('user_type') == 'doctor':
-        return 'doctor_home.html'
+        visits = get_visits_for_doctor(request.session.get('email'))
+        context = {'visits': visits}
+        template_name = 'doctor_home.html'
+    return template_name, context
 
 
 def add_visit(request):
-    # doctor id must be changed (session)
-    doctor = "anna.nowak@mail.com"
+    doctor_mail = request.session.get('email')
 
     if request.method == 'POST':
         address_form = AddressForm(request.POST)
         visit_form = VisitForm(request.POST)
 
         if address_form.is_valid() and visit_form.is_valid():
-            adress = address_form.save()
+            address = address_form.save()
 
-            address_name = adress.all().values().values_list()[0][1]
+            address_name = address.all().values().values_list()[0][1]
 
             visit_form.cleaned_data['address'] = address_name
-            visit_form.cleaned_data['doctor'] = doctor
+            visit_form.cleaned_data['doctor'] = doctor_mail
             visit_form.save()
 
-            return redirect('/appointments/doctor_home/')
+            return redirect('/appointments/home/')
         else:
             print(address_form.errors, visit_form.errors)
     else:
@@ -102,3 +107,33 @@ def add_doctor(request):
         form = DoctorForm()
 
     return render(request, 'add_doctor.html', {'form': form})
+
+
+def get_visits_for_doctor(doctor_email):
+    doctor_visits = Visit.objects.filter(doctor=doctor_email)
+    list_v = list(doctor_visits.all().values().values_list())
+    visits = []
+    for v in list_v:
+
+        if v[4] is not None:
+            patient = Patient.objects.filter(email=v[4])
+            patient_values = list(patient.all().values().values_list())
+            first_name_patient = patient_values[0][1]
+            last_name_patient = patient_values[0][2]
+        else:
+            first_name_patient = "---"
+            last_name_patient = "---"
+
+        clinic_name = v[3]
+        address = Address.objects.filter(name=clinic_name).all().values().values_list()
+        address_street = f"ul. %s" % (address[0][2])
+        address_city = f"%s %s" % (address[0][4], address[0][3])
+        time = v[1].time().strftime("%H:%M")
+        date = v[1].date().strftime("%d/%m/%Y")
+
+        visit = {'first_name_patient': first_name_patient, 'last_name_patient': last_name_patient,
+                 'clinic_name': clinic_name, 'address_street': address_street, 'address_city': address_city,
+                 'time': time, 'date': date }
+        visits.append(visit)
+
+    return visits
