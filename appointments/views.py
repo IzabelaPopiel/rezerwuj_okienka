1aa29f8e97a0a335ef20091f9afc6ac480f5dcde
Paula <236722@student.pwr.edu.pl>
2020-12-30 10:36:04 +0100

    accepting slots, displaying "my visits" in the patient's application, booking appointments in "search for an appointment", displaying future appointments in tables

--- appointments/views.py
+++ appointments/views.py
@@ -1,13 +1,17 @@
+import json
+from datetime import datetime
+
 from bson import json_util, ObjectId
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from pymongo import MongoClient
+
 from appointments.forms import PatientForm, LoginForm, AddressForm, Doctor, DoctorForm, VisitForm, MedicalSpecialtyForm, \
     AlertForm
 from appointments.models import Visit, Address, Patient, MedicalSpecialty, Alert
-from django.contrib import messages
-import json
 from reservationsystem import config
 
 
@@ -69,13 +73,51 @@ def is_already_logged(request):
 
 def redirect_template(request):
     if request.session.get('user_type') == 'patient':
-        context = {'home_page': 'active'}
+        patient_mail = request.session.get('email')
+        visits = get_patient_visits(patient_mail)
+        context = {'home_page': 'active', 'visits': visits}
         template_name = 'patient_home.html'
     elif request.session.get('user_type') == 'doctor':
         visits = get_visits_for_doctor(request.session.get('email'))
         context = {'visits': visits}
         template_name = 'doctor_home.html'
     return template_name, context
+
+
+
+def get_patient_visits(patient_mail):
+    visits = []
+    visit_collection = MongoClient(config.host)["appointmentSystem"]["appointments_visit"]
+    matching_visit = visit_collection.find({'patient': patient_mail})
+
+    for v in matching_visit:
+        now = datetime.now()
+        if v['date'] > now:
+
+
+            doctor = Doctor.objects.filter(email=v["doctor"])
+            doctor_values = list(doctor.all().values().values_list())
+
+            first_name_doctor = doctor_values[0][1]
+            last_name_doctor = doctor_values[0][2]
+            medical_specialty = doctor_values[0][3]
+
+            clinic_name = v['address']
+            address = Address.objects.filter(name=clinic_name).all().values().values_list()
+            address_street = f"ul. %s" % (address[0][2])
+            address_city = f"%s %s" % (address[0][4], address[0][3])
+            time = v['date'].time().strftime("%H:%M")
+            date = v['date'].date().strftime("%d/%m/%Y")
+            # date_time = v[1].date().strftime("%Y-%m-%d") + " " + time
+            visit_id = v["_id"]
+
+            visit = {'medical_specialty': medical_specialty, 'first_name_doctor': first_name_doctor,
+                     'last_name_doctor': last_name_doctor,
+                     'clinic_name': clinic_name, 'address_street': address_street, 'address_city': address_city,
+                     'time': time, 'date': date,'visit_id': visit_id }
+            visits.append(visit)
+
+    return visits
 
 
 def add_visit(request):
@@ -120,6 +162,7 @@ def set_slots_for_patients(visit_pk, doctor_email, address):
 
     for a in alerts:
         p_email = a[3]
+
         patients_emails.append(p_email)
 
         patient = Patient.objects.filter(email=p_email)
@@ -132,13 +175,42 @@ def set_slots_for_patients(visit_pk, doctor_email, address):
 
     # here will be send emails
 
+def remove_slots_help(visit_id):
+    booked = False
+    patient_collection = Patient.objects.all().values('email')
+    for p in patient_collection:
+
+            patient = Patient.objects.filter(email=p['email'])
+            patient_slots_str = getattr(patient.first(), 'slots')
+
+            if patient_slots_str is not None:
+                patient_slots = parse_json(patient_slots_str)["slots"]
+                try:
+                    patient_slots.remove({'$oid': str(visit_id)})
+                    if patient.update(slots=parse_json({"slots": patient_slots})):
+                        booked = True
+                except ValueError:
+                    print("ValueError")
+    return booked
+
 
 def remove_visit(request, date_time):
     doctor_mail = request.session.get('email')
-    visit = Visit.objects.filter(doctor=doctor_mail, date=date_time)
+    visit = Visit.objects.get(doctor=doctor_mail, date=date_time)
 
     if request.method == 'POST':
-        visit.delete()
+        visit_collection = MongoClient(config.host)["appointmentSystem"]["appointments_visit"]
+        matching_visit = visit_collection.find_one({'doctor': doctor_mail, 'date': visit.date})
+        visit_id = matching_visit["_id"]
+        booked = remove_slots_help(visit_id)
+
+
+        if booked:
+            messages.success(request, "Pomyślnie usunięto wizytę")
+        else:
+            messages.error(request, "Wystąpił błąd, spróbuj ponownie")
+
+        visit_collection.remove(matching_visit)
         return redirect('/appointments/home/')
     else:
 
@@ -151,12 +223,10 @@ def search_visit(request):
     for m_specialty in medical_specialty_list:
         select.append(m_specialty[1])
     visits = get_free_visits()
-    # return render(request,'search_visit.html')
+
     context = {'visits': visits, 'specialties': select}
     template_name = 'search_visit.html'
     return render(request, template_name, context)
-    # return template_name, context
-    # return render(request, 'search_visit.html')
 
 
 @login_required(login_url='/admin/login/')
@@ -178,28 +248,30 @@ def get_visits_for_doctor(doctor_email):
     list_v = list(doctor_visits.all().values().values_list())
     visits = []
     for v in list_v:
+        now = timezone.now()
+        if v[1] > now:
 
-        if v[4] is not None:
-            patient = Patient.objects.filter(email=v[4])
-            patient_values = list(patient.all().values().values_list())
-            first_name_patient = patient_values[0][1]
-            last_name_patient = patient_values[0][2]
-        else:
-            first_name_patient = "---"
-            last_name_patient = "---"
+            if v[4] is not None:
+                patient = Patient.objects.filter(email=v[4])
+                patient_values = list(patient.all().values().values_list())
+                first_name_patient = patient_values[0][1]
+                last_name_patient = patient_values[0][2]
+            else:
+                first_name_patient = "---"
+                last_name_patient = "---"
 
-        clinic_name = v[3]
-        address = Address.objects.filter(name=clinic_name).all().values().values_list()
-        address_street = f"ul. %s" % (address[0][2])
-        address_city = f"%s %s" % (address[0][4], address[0][3])
-        time = v[1].time().strftime("%H:%M")
-        date = v[1].date().strftime("%d/%m/%Y")
-        date_time = v[1].date().strftime("%Y-%m-%d") + " " + time
+            clinic_name = v[3]
+            address = Address.objects.filter(name=clinic_name).all().values().values_list()
+            address_street = f"ul. %s" % (address[0][2])
+            address_city = f"%s %s" % (address[0][4], address[0][3])
+            time = v[1].time().strftime("%H:%M")
+            date = v[1].date().strftime("%d/%m/%Y")
+            date_time = v[1].date().strftime("%Y-%m-%d") + " " + time
 
-        visit = {'first_name_patient': first_name_patient, 'last_name_patient': last_name_patient,
-                 'clinic_name': clinic_name, 'address_street': address_street, 'address_city': address_city,
-                 'time': time, 'date': date, 'dateTime': date_time}
-        visits.append(visit)
+            visit = {'first_name_patient': first_name_patient, 'last_name_patient': last_name_patient,
+                     'clinic_name': clinic_name, 'address_street': address_street, 'address_city': address_city,
+                     'time': time, 'date': date, 'dateTime': date_time}
+            visits.append(visit)
 
     return visits
 
@@ -249,19 +321,19 @@ def patient_alerts(request):
     cards_text = []
 
     for slot in patient_slots:
-        visit_id = slot['oid']
+        visit_id = slot['$oid']
         matching_visit = visit_collection.find_one({'_id': ObjectId(visit_id)})
+        if matching_visit is not None:
+            date_format = matching_visit["date"].date().strftime("%Y-%m-%d") + " godz. " + matching_visit[
+                "date"].time().strftime("%H:%M")
+            specialty = getattr(Doctor.objects.filter(email=matching_visit["doctor"]).first(), 'medical_Specialty')
+            clinic_address = Address.objects.filter(name=matching_visit["address"]).first()
+            full_address = matching_visit["address"] + " ul. " + getattr(clinic_address, 'street') + ", " + getattr(
+                clinic_address, 'city')
 
-        date_format = matching_visit["date"].date().strftime("%Y-%m-%d") + " godz. " + matching_visit[
-            "date"].time().strftime("%H:%M")
-        specialty = getattr(Doctor.objects.filter(email=matching_visit["doctor"]).first(), 'medical_Specialty')
-        clinic_address = Address.objects.filter(name=matching_visit["address"]).first()
-        full_address = matching_visit["address"] + " ul. " + getattr(clinic_address, 'street') + ", " + getattr(
-            clinic_address, 'city')
-
-        cards_text.append(
-            {'specialty': specialty, 'doctor': matching_visit["doctor"], 'datatime': date_format,
-             'address': full_address, 'visit_id': visit_id})
+            cards_text.append(
+                {'specialty': specialty, 'doctor': matching_visit["doctor"], 'datatime': date_format,
+                 'address': full_address, 'visit_id': visit_id})
 
     context = {'alert_page': 'active', 'alert_form': alert_form,
                'medical_specialties_form': medical_specialties_form,
@@ -306,8 +378,37 @@ def remove_slot(request, visit_id):
         return render(request, 'patient_alerts.html')
 
 
+def book_visit(request, visit_id):
+    patient_mail = request.session.get('email')
+    if request.method == 'POST':
+        visit_collection = MongoClient(config.host)["appointmentSystem"]["appointments_visit"]
+        visit_collection.update_one({'_id': ObjectId(visit_id)},{"$set":{"patient":patient_mail}})
+        remove_slots_help(visit_id)
+        return redirect('/appointments/home/search_visit')
+    else:
+        return render(request, 'search_visit.html')
+
+
 def accept_slot(request, visit_id):
-    return redirect('/appointments/home/alerts/')
+    patient_mail = request.session.get('email')
+
+    if request.method == 'POST':
+        visit_collection = MongoClient(config.host)["appointmentSystem"]["appointments_visit"]
+        matching_visit = visit_collection.find_one({'_id': ObjectId(visit_id)})
+
+        if matching_visit['patient'] == None:
+            visit_collection.update_one({'_id': ObjectId(visit_id)},{"$set":{"patient":patient_mail}})
+            booked = remove_slots_help(visit_id)
+
+        if booked:
+            messages.success(request, "Pomyślnie zarezerwowano okienko")
+        else:
+            messages.error(request, "Wystąpił błąd, spróbuj ponownie")
+
+        return redirect('/appointments/home/alerts/')
+    else:
+
+        return render(request, 'patient_alerts.html')
 
 
 def search_visit(request):
@@ -322,33 +423,34 @@ def search_visit(request):
 
 
 def get_free_visits():
-    visits = Visit.objects.filter(patient=None)
-    list_v = list(visits.all().values().values_list())
     visits = []
-    print('tutaj')
-    for v in list_v:
-        print(v)
-        doctor = Doctor.objects.filter(email=v[2])
-        doctor_values = list(doctor.all().values().values_list())
-        print(doctor_values)
-        # first_name_doctor = 'Jan'
-        # last_name_doctor = 'Kowalski'
-        first_name_doctor = doctor_values[0][1]
-        last_name_doctor = doctor_values[0][2]
-        medical_specialty = doctor_values[0][3]
+    visit_collection = MongoClient(config.host)["appointmentSystem"]["appointments_visit"]
+    matching_visit = visit_collection.find({'patient': None})
 
-        clinic_name = v[3]
-        address = Address.objects.filter(name=clinic_name).all().values().values_list()
-        address_street = f"ul. %s" % (address[0][2])
-        address_city = f"%s %s" % (address[0][4], address[0][3])
-        time = v[1].time().strftime("%H:%M")
-        date = v[1].date().strftime("%d/%m/%Y")
-        date_time = v[1].date().strftime("%Y-%m-%d") + " " + time
+    for v in matching_visit:
+
+        now = datetime.now()
+        if v['date'] > now:
+            doctor = Doctor.objects.filter(email=v["doctor"])
+            doctor_values = list(doctor.all().values().values_list())
+
+            first_name_doctor = doctor_values[0][1]
+            last_name_doctor = doctor_values[0][2]
+            medical_specialty = doctor_values[0][3]
+
+            clinic_name = v['address']
+            address = Address.objects.filter(name=clinic_name).all().values().values_list()
+            address_street = f"ul. %s" % (address[0][2])
+            address_city = f"%s %s" % (address[0][4], address[0][3])
+            time = v['date'].time().strftime("%H:%M")
+            date = v['date'].date().strftime("%d/%m/%Y")
+            # date_time = v[1].date().strftime("%Y-%m-%d") + " " + time
+            visit_id = v["_id"]
 
         visit = {'medical_specialty': medical_specialty, 'first_name_doctor': first_name_doctor,
                  'last_name_doctor': last_name_doctor,
                  'clinic_name': clinic_name, 'address_street': address_street, 'address_city': address_city,
-                 'time': time, 'date': date, }
+                 'time': time, 'date': date,'visit_id': visit_id }
         visits.append(visit)
 
     return visits
